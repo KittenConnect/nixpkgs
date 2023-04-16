bcb5684575143026e451f5c87257bb26e8800192
Doron Behar <doron.behar@gmail.com>
2023-04-16 18:48:57 +0300

    sdr-j-fm: init at 3.16-unstable-2023-12-07

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24257,6 +24257,8 @@ with pkgs;
 
   sdrplay = callPackage ../applications/radio/sdrplay { };
 
+  sdr-j-fm = libsForQt5.callPackage ../applications/radio/sdr-j-fm { };
+
   sdrpp = callPackage ../applications/radio/sdrpp {
     inherit (darwin.apple_sdk.frameworks) AppKit;
   };
