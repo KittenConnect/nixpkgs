5d8604c34cea569da2b85e00c38a3350e864eec6
Rodney Lorrimar <dev@rodney.id.au>
2024-02-11 08:52:17 +0800

    libadwaita: Add desktop-file-utils optional build dep

--- pkgs/development/libraries/libadwaita/default.nix
+++ pkgs/development/libraries/libadwaita/default.nix
@@ -14,6 +14,7 @@
 , gtk4
 , gnome
 , gsettings-desktop-schemas
+, desktop-file-utils
 , xvfb-run
 , AppKit
 , Foundation
@@ -47,6 +48,7 @@ stdenv.mkDerivation (finalAttrs: {
     sassc
     vala
     gobject-introspection
+    desktop-file-utils  # for validate-desktop-file
   ];
 
   mesonFlags = [
