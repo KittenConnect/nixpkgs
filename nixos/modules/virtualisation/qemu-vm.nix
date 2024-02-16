76a3f51619032fda8ea75096c42cfa3a34ef2ddf
Anders Johan Jamtli <anders@jamtli.no>
2024-02-16 06:26:26 +0100

    nixos/qemu-vm: changed default security model
    Changed the default security model for shared directories from 'none' to
    the 'mapped-xattr'. QEMU recommends using this option for its security and
    reliability benefits.

--- nixos/modules/virtualisation/qemu-vm.nix
+++ nixos/modules/virtualisation/qemu-vm.nix
@@ -464,7 +464,7 @@ in
             };
             options.securityModel = mkOption {
               type = types.enum [ "passthrough" "mapped-xattr" "mapped-file" "none" ];
-              default = "none";
+              default = "mapped-xattr";
               description = lib.mdDoc ''
                 The security model to use for this share:
 
