264cf2711939d01151093b9acfb40fd46c4dacb4
t4ccer <t4ccer@gmail.com>
2024-02-24 14:37:22 -0700

    python3Packages.graphlib-backport: init at 1.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5001,6 +5001,8 @@ self: super: with self; {
 
   graphene-django = callPackage ../development/python-modules/graphene-django { };
 
+  graphlib-backport = callPackage ../development/python-modules/graphlib-backport { };
+
   graphqlclient= callPackage ../development/python-modules/graphqlclient { };
 
   graphql-core = callPackage ../development/python-modules/graphql-core { };
