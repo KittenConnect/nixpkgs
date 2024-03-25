9cda56832bb0ed2774e8ab38f4c5e2eb8a670907
José Luis Lafuente <jl@lafuente.dev>
2024-03-25 21:02:28 +0100

    jextract: unstable-2023-11-27 -> unstable-2024-03-13
    Closes #293102

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25396,8 +25396,6 @@ with pkgs;
 
   fastjar = callPackage ../development/tools/java/fastjar { };
 
-  jextract = callPackage ../development/tools/java/jextract { };
-
   httpunit = callPackage ../development/libraries/java/httpunit { };
 
   javaCup = callPackage ../development/libraries/java/cup {
