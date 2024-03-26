f884634644b23f31f6196d32196674cacc792e96
Robin Hack <hack.robin@gmail.com>
2024-03-26 21:10:55 +0100

    maintainers: add marmolak

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -12200,6 +12200,12 @@
     githubId = 26470037;
     name = "Markus Kowalewski";
   };
+  marmolak = {
+    email = "hack.robin@gmail.com";
+    github = "marmolak";
+    githubId = 1709273;
+    name = "Robin Hack";
+  };
   marsam = {
     github = "marsam";
     githubId = 65531;
