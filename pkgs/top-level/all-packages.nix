3e2bb3dbce4e1720cb88387ecc00af4ffac4405f
rewine <luhongxu@deepin.org>
2024-03-28 18:36:34 +0800

    wlroots_0_15: remove

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32084,7 +32084,6 @@ with pkgs;
   super-productivity = callPackage ../applications/office/super-productivity { };
 
   inherit (callPackages ../development/libraries/wlroots {})
-    wlroots_0_15
     wlroots_0_16
     wlroots_0_17
     wlroots;
