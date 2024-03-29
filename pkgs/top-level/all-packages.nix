1c142e26fb627cb90ba2f7a42bc9bdfd3235ca62
linsui <linsui@inbox.lv>
2024-03-29 16:59:48 +0800

    pot: 2.7.4 -> 2.7.9

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34193,8 +34193,6 @@ with pkgs;
 
   pop-launcher = callPackage ../applications/misc/pop-launcher { };
 
-  pot = callPackage ../applications/misc/pot { };
-
   pothos = libsForQt5.callPackage ../applications/radio/pothos { };
 
   potrace = callPackage ../applications/graphics/potrace { };
