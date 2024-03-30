540b9d0780c3c2f95951646b643c0dd18fa34eb8
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2024-03-30 19:38:20 +0100

    inochi-creator: init at 0.8.4, inochi-session: init at 0.8.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19052,6 +19052,9 @@ with pkgs;
 
   inotify-tools = callPackage ../development/tools/misc/inotify-tools { };
 
+  inherit (callPackage ../applications/misc/inochi2d { })
+    inochi-creator inochi-session;
+
   intel-gpu-tools = callPackage ../development/tools/misc/intel-gpu-tools { };
 
   insomnia = callPackage ../development/web/insomnia { };
