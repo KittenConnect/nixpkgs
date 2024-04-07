1132e82bb770c52d08f988d05b506685de61467f
stupidcomputer <ryan@beepboop.systems>
2024-04-06 19:47:58 -0500

    maintainers: remove avery

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -1990,12 +1990,6 @@
     githubId = 687218;
     name = "averelld";
   };
-  avery = {
-    email = "averyl+nixos@protonmail.com";
-    github = "AveryLychee";
-    githubId = 9147625;
-    name = "Avery Lychee";
-  };
   avh4 = {
     email = "gruen0aermel@gmail.com";
     github = "avh4";
