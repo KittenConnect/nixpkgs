31cc04dd78dec116dc77e0a2e890dc7ffa23b514
Enric Morales <me@enric.me>
2024-04-09 15:48:55 +0200

    Add myself as maintainer

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10524,6 +10524,12 @@
     githubId = 845652;
     name = "Kier Davis";
   };
+  kiike = {
+    email = "me@enric.me";
+    github = "kiike";
+    githubId = 464625;
+    name = "Enric Morales";
+  };
   kilianar = {
     email = "mail@kilianar.de";
     github = "kilianar";
