c08ea201de53780812156b4ab961eae538a82710
Julius Michaelis <gitter@liftm.de>
2024-04-14 22:03:24 +0900

    maintainers: add jcaesar

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -9095,6 +9095,12 @@
     github = "joshua-cooper";
     githubId = 35612334;
   };
+  jcaesar = {
+    name = "Julius Michaelis";
+    matrix = "@julius:mtx.liftm.de";
+    github = "jcaesar";
+    githubId = 1753388;
+  };
   jceb = {
     name = "Jan Christoph Ebersbach";
     email = "jceb@e-jc.de";
