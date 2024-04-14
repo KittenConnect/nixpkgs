d2c6b32bceeb057b4c8e4f536f8c2935d4184eb1
Sarah Clark <seclark@nextquestion.net>
2024-04-13 18:25:42 -0700

    maintainers: corrected email address (#303922)
    Co-authored-by: sarahec <sarahec@nextquestion.net>

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -17679,7 +17679,7 @@
     name = "Sanskar Gurdasani";
   };
   sarahec = {
-    email = "sarahec@nextquestion.net";
+    email = "seclark@nextquestion.net";
     github = "sarahec";
     githubId = 11277967;
     name = "Sarah Clark";
