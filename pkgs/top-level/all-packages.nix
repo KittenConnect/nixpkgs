7d4928871172e381f38935c1dc7c2527c80756fd
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-04-17 00:50:55 +0200

    ffado: remove unused kernel argument, cleanup python env creation

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27193,9 +27193,7 @@ with pkgs;
 
   fatrace = callPackage ../os-specific/linux/fatrace { };
 
-  ffado = libsForQt5.callPackage ../os-specific/linux/ffado {
-    inherit (linuxPackages) kernel;
-  };
+  ffado = libsForQt5.callPackage ../os-specific/linux/ffado { };
   libffado = ffado;
 
   fbterm = callPackage ../os-specific/linux/fbterm { };
