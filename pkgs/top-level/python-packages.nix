108beebf6f9385cf3a1407e53b7df5c00508080d
Gaetan Lepage <gaetan@glepage.com>
2024-04-16 22:36:12 +0200

    python311Packages.timing-asgi: init at 0.3.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15057,6 +15057,8 @@ self: super: with self; {
 
   timezonefinder = callPackage ../development/python-modules/timezonefinder { };
 
+  timing-asgi = callPackage ../development/python-modules/timing-asgi { };
+
   timm = callPackage ../development/python-modules/timm { };
 
   tiny-cuda-nn = toPythonModule (pkgs.tiny-cuda-nn.override {
