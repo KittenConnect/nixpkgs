a03bb43e0d85ceb44fa2571206e421231cbca5d9
Gautier DI FOLCO <gautier.difolco@gmail.com>
2024-04-20 11:22:10 +0200

    maintainers: add gdifolco

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -7220,6 +7220,12 @@
     githubId = 40209356;
     name = "Gabriel Doriath Döhler";
   };
+  gdifolco = {
+    email = "gautier.difolco@gmail.com";
+    github = "blackheaven";
+    githubId = 1362807;
+    name = "Gautier Di Folco";
+  };
   gdinh = {
     email = "nix@contact.dinh.ai";
     github = "gdinh";
