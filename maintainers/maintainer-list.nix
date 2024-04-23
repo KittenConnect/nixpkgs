828bae95ee870185c19d46c8dd6d2f489f141914
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2024-04-23 23:25:05 +0300

    metals: add jpaju as maintainer

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -9898,6 +9898,11 @@
     github = "jpagex";
     githubId = 635768;
   };
+  jpaju = {
+    name = "Jaakko Paju";
+    github = "jpaju";
+    githubId = 36770267;
+  };
   jpas = {
     name = "Jarrod Pas";
     email = "jarrod@jarrodpas.com";
