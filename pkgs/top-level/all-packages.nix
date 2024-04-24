45505c0529f0d70ca6ae0c31da49e1546cbc1f86
Joachim Ernst <joachim.ernst@helsinki-systems.de>
2024-04-24 16:18:49 +0200

    pdns-recursor: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27972,8 +27972,6 @@ with pkgs;
 
   dnsdist = callPackage ../servers/dns/dnsdist { };
 
-  pdns-recursor = callPackage ../servers/dns/pdns-recursor { };
-
   powertop = callPackage ../os-specific/linux/powertop { };
 
   pps-tools = callPackage ../os-specific/linux/pps-tools { };
