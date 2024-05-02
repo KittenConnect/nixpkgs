b80f90c48a6b24e02952e240455deadee378f868
Anderson Torres <torres.anderson.85@protonmail.com>
2024-05-01 21:29:35 -0300

    SDL_Pango: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24226,8 +24226,6 @@ with pkgs;
 
   SDL_net = callPackage ../development/libraries/SDL_net { };
 
-  SDL_Pango = callPackage ../development/libraries/SDL_Pango { };
-
   SDL_sound = callPackage ../development/libraries/SDL_sound { };
 
   SDL_stretch= callPackage ../development/libraries/SDL_stretch { };
