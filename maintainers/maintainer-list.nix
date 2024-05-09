9d98b350cedde315503a4545f8189a28f22bc14d
Joan Massachs <22916782+JoanMassachs@users.noreply.github.com>
2024-05-09 09:19:11 +0200

    maintainers: add joanmassachs

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -9890,6 +9890,11 @@
     githubId = 1102396;
     name = "Jussi Maki";
   };
+  joanmassachs = {
+    github = "joanmassachs";
+    githubId = 22916782;
+    name = "Joan Massachs";
+  };
   joaquintrinanes = {
     email = "hi@joaquint.io";
     github = "JoaquinTrinanes";
