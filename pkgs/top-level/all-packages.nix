585bdbd33f4f90f4ca814cedec3a7bcaadc23334
Sigmanificient <edhyjox@gmail.com>
2024-05-13 01:28:56 +0200

    python3Packages.cq-editor: remove

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30198,8 +30198,6 @@ with pkgs;
 
   cplay-ng = callPackage ../applications/audio/cplay-ng { };
 
-  cq-editor = libsForQt5.callPackage ../applications/graphics/cq-editor { };
-
   cqrlog = callPackage ../applications/radio/cqrlog {
     hamlib = hamlib_4;
   };
