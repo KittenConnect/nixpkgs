a1d43e9790d2723b986178c81f676049d77956d5
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2024-05-14 21:51:42 +0200

    python312Packages.torchsummary: init at 1.5.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15324,6 +15324,8 @@ self: super: with self; {
 
   torchsde = callPackage ../development/python-modules/torchsde { };
 
+  torchsummary = callPackage ../development/python-modules/torchsummary { };
+
   torchvision = callPackage ../development/python-modules/torchvision { };
 
   torchvision-bin = callPackage ../development/python-modules/torchvision/bin.nix {
