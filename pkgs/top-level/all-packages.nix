51552bbc6ff8b78fde17c316186e75c427dcd8a1
isabel <isabel@isabelroses.com>
2024-05-14 18:39:23 +0100

    goflow2: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1843,8 +1843,6 @@ with pkgs;
 
   goflow = callPackage ../tools/networking/goflow { };
 
-  goflow2 = callPackage ../tools/networking/goflow2 { };
-
   gofu = callPackage ../applications/misc/gofu { };
 
   godspeed = callPackage ../tools/networking/godspeed { };
