35ef1aca1ec51b3ab5f8de4767b611527a15be81
nhnn <nhnn@disroot.org>
2024-05-16 18:12:34 +0300

    maintainers: add nhnn

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -14497,6 +14497,12 @@
     githubId = 399535;
     name = "Niklas Hambüchen";
   };
+  nhnn = {
+    matrix = "@nhnn:nhnn.dev";
+    github = "thenhnn";
+    githubId = 162156666;
+    name = "nhnn";
+  };
   nhooyr = {
     email = "anmol@aubble.com";
     github = "nhooyr";
