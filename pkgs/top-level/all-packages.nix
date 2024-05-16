3b7997ddb91caf4e485d0f217363da71f51fe656
Markus Kowalewski <markus.kowalewski@gmail.com>
2024-05-16 13:04:35 +0200

    saunasfs: init at 4.2.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2884,6 +2884,10 @@ with pkgs;
     inherit (darwin) autoSignDarwinBinariesHook;
   };
 
+  saunafs = callPackage ../by-name/sa/saunafs/package.nix {
+    boost = boost185;
+  };
+
   mucommander = callPackage ../applications/file-managers/mucommander { };
 
   nnn = callPackage ../applications/file-managers/nnn { };
