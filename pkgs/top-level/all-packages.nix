fb952824678f7ef87d476926a3e6b74df7a21ed4
Anderson Torres <torres.anderson.85@protonmail.com>
2024-05-22 14:33:40 -0300

    mpv: move wrapper under mpv-unwrapped.passthru
    And refactor to fit.
    
    wrapper

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32963,11 +32963,10 @@ with pkgs;
     inherit lua;
   };
 
-  shaka-packager = callPackage ../applications/video/shaka-packager { };
+  # Wrap avoiding rebuild
+  mpv = mpv-unwrapped.wrapper { mpv = mpv-unwrapped; };
 
-  # Wraps without triggering a rebuild
-  wrapMpv = callPackage ../applications/video/mpv/wrapper.nix { };
-  mpv = wrapMpv mpv-unwrapped { };
+  shaka-packager = callPackage ../applications/video/shaka-packager { };
 
   mpvpaper = callPackage ../tools/wayland/mpvpaper { };
 
