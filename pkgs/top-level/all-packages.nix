cf2481de63373e592d9eed93a9e98e00f3dfd626
linuxissuper <74221543+linuxissuper@users.noreply.github.com>
2024-05-22 14:33:57 +0200

    sirikali: 1.5.1 -> 1.6.0 (#310323)
    move to by-name folder
    build with qt6 instead of qt5
    
    Co-authored-by: linuxissuper <m+nix@linuxistcool.de>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -41103,8 +41103,6 @@ with pkgs;
 
   dict-cc-py = callPackage ../applications/misc/dict-cc-py { };
 
-  sirikali = libsForQt5.callPackage ../tools/security/sirikali { };
-
   wpm = callPackage ../applications/misc/wpm { };
 
   weggli = callPackage ../tools/security/weggli { };
