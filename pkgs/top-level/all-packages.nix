597d1494b812ff7dfd9cd022138aad2e92f463d4
Dawid Dziurla <dawidd0811@gmail.com>
2024-05-25 13:42:52 +0200

    all-packages: add gpt4all-cuda

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8558,6 +8558,10 @@ with pkgs;
 
   gparted = callPackage ../tools/misc/gparted { };
 
+  gpt4all-cuda = gpt4all.override {
+    cudaSupport = true;
+  };
+
   gpt2tc = callPackage ../tools/text/gpt2tc { };
 
   gptcommit = callPackage ../development/tools/gptcommit {
