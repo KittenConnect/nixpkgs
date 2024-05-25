640aea2201298c5002c29cf48bcbdba278d3d62e
Yueh-Shun Li <shamrocklee@posteo.net>
2024-05-25 16:45:23 +0800

    libchewing: move Nix expression under pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20449,8 +20449,6 @@ with pkgs;
 
   libchardet = callPackage ../development/libraries/libchardet { };
 
-  libchewing = callPackage ../development/libraries/libchewing { };
-
   libchipcard = callPackage ../development/libraries/aqbanking/libchipcard.nix { };
 
   libcrafter = callPackage ../development/libraries/libcrafter { };
