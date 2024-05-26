180dc7526337d98f497cdf9a00a9e35903261208
Moraxyc <i@qaq.li>
2024-05-27 01:26:46 +0800

    python312Packages.noneprompt: init at 0.1.9

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8656,6 +8656,8 @@ self: super: with self; {
 
   nomadnet = callPackage ../development/python-modules/nomadnet { };
 
+  noneprompt = callPackage ../development/python-modules/noneprompt {  };
+
   nox = callPackage ../development/python-modules/nox { };
 
   nanomsg-python = callPackage ../development/python-modules/nanomsg-python {
