51e545b038ca06f47a2275a592d2f4937642ed25
Nadir Ishiguro <23151917+nadir-ishiguro@users.noreply.github.com>
2024-05-27 23:12:06 +0200

    polychromatic: 0.8.0 -> 0.9.1
    Changelogs:
    
      - https://github.com/polychromatic/polychromatic/releases/tag/v0.9.1
      - https://github.com/polychromatic/polychromatic/releases/tag/v0.9.0
      - https://github.com/polychromatic/polychromatic/releases/tag/v0.8.4
      - https://github.com/polychromatic/polychromatic/releases/tag/v0.8.3
      - https://github.com/polychromatic/polychromatic/releases/tag/v0.8.2
      - https://github.com/polychromatic/polychromatic/releases/tag/v0.8.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -29037,7 +29037,7 @@ with pkgs;
 
   poly = callPackage ../data/fonts/poly { };
 
-  polychromatic = libsForQt5.callPackage ../applications/misc/polychromatic { };
+  polychromatic = qt6Packages.callPackage ../applications/misc/polychromatic { };
 
   polytopes_db = callPackage ../data/misc/polytopes_db { };
 
