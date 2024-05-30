c9e432de4c142934ed938215ae7dacc34d976203
Sigmanificient <edhyjox@gmail.com>
2024-05-30 22:51:47 +0200

    python312Packages.paperbush: init at 0.2.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9456,6 +9456,8 @@ self: super: with self; {
 
   panphon = callPackage ../development/python-modules/panphon { };
 
+  paperbush = callPackage ../development/python-modules/paperbush { };
+
   papermill = callPackage ../development/python-modules/papermill { };
 
   openpaperwork-core = callPackage ../applications/office/paperwork/openpaperwork-core.nix { };
