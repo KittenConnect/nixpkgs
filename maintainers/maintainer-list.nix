ed9c18d014c8f75e8964a0cdcd89d6a3566bee70
Oleksii Dorozhkin <oleksii.dorozhkin@getflowbox.com>
2024-05-31 21:30:57 +0200

    maintainers: add vglfr

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -21272,6 +21272,12 @@
     githubId = 245573;
     name = "Dmitry Kalinkin";
   };
+  vglfr = {
+    email = "vf.velt@gmail.com";
+    github = "vglfr";
+    githubId = 20283252;
+    name = "vglfr";
+  };
   vgskye = {
     name = "Skye Green";
     email = "me@skye.vg";
