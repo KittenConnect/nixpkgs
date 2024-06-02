a8fbbfa7bec0d68616b43ea0233e36cc9d5b48b1
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-06-03 03:41:08 +0800

    python3Packages.libnbd: init at 1.18.2 (#313358)
    Co-authored-by: Anderson Torres <torres.anderson.85@protonmail.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6692,6 +6692,11 @@ self: super: with self; {
     inherit (pkgs) libsodium;
   };
 
+  libnbd = toPythonModule (pkgs.libnbd.override {
+    buildPythonBindings = true;
+    python3 = python;
+  });
+
   libpcap = callPackage ../development/python-modules/libpcap {
     pkgsLibpcap = pkgs.libpcap; # Needs the C library
   };
