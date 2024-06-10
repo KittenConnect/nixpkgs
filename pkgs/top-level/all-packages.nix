1d8aee19a7c7fd334648024609dea78227a77f4a
Doron Behar <doron.behar@gmail.com>
2024-06-10 19:38:55 +0300

    taskwarrior2: rename from taskwarrior, with a warning alias
    Also remove an outdated link in `taskserver/default.md`.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -33861,8 +33861,6 @@ with pkgs;
 
   tasktimer = callPackage ../applications/misc/tasktimer { };
 
-  taskwarrior = callPackage ../applications/misc/taskwarrior { };
-
   taskwarrior-tui = callPackage ../applications/misc/taskwarrior-tui { };
 
   dstask = callPackage ../applications/misc/dstask { };
