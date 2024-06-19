601c39e84605097dff7273259dc1854335c1ef7e
Anderson Torres <torres.anderson.85@protonmail.com>
2024-06-19 10:10:10 -0300

    qmplay2: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -33330,8 +33330,6 @@ with pkgs;
 
   qmediathekview = libsForQt5.callPackage ../applications/video/qmediathekview { };
 
-  qmplay2 = callPackage ../applications/video/qmplay2 { };
-
   qmidiarp = callPackage ../applications/audio/qmidiarp { };
 
   qmidinet = libsForQt5.callPackage ../applications/audio/qmidinet { };
