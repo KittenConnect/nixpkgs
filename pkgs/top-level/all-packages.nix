7dfdae923aacdaa018f38034016d62f21db26989
Sirio Balmelli <sirio@b-ad.ch>
2024-06-19 13:35:20 +0000

    odoo: 16.0.20231024 -> 17.0.20240507
    Preserve 'odoo16' on the same pattern as existing 'odoo15'
    
    Signed-off-by: Sirio Balmelli <sirio@b-ad.ch>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5971,6 +5971,7 @@ with pkgs;
   };
 
   odoo = callPackage ../applications/finance/odoo { };
+  odoo16 = callPackage ../applications/finance/odoo/odoo16.nix { };
   odoo15 = callPackage ../applications/finance/odoo/odoo15.nix { };
 
   odafileconverter = libsForQt5.callPackage ../applications/graphics/odafileconverter { };
