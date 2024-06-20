8f708892b45595f5a91634351616236b78ef9afb
superherointj <5861043+superherointj@users.noreply.github.com>
2024-06-20 13:46:43 -0300

    xsimd: move package to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24914,8 +24914,6 @@ with pkgs;
 
   xlslib = callPackage ../development/libraries/xlslib { };
 
-  xsimd = callPackage ../development/libraries/xsimd { };
-
   xtensor = callPackage ../development/libraries/xtensor { };
 
   xtl = callPackage ../development/libraries/xtl { };
