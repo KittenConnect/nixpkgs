101099462924b50695e3af85bf732ed9996c6a58
Alex James <git@alextjam.es>
2024-06-22 15:01:29 -0500

    convmv: add Darwin support, reformat & migrate to pkgs/by-name
    Add support for Darwin to convmv. This required disabling the tests as
    APFS requires filenames to be valid UTF-8[^1]. This will also affect
    Linux if a filesystem that enforces valid UTF-8 is used (such as ZFS
    with `utf8only=on`).
    
    While we're here, make some changes to clean up the derivation:
    
    - Replace use of `rec` with `finalAttrs` pattern
    - Replace `fetchurl` with `fetchzip`
    - Added separate `man` output
    - Replace `preBuild` hook with explicit `makeFlags` initialization
    - Replace explicit `patchPhase` override with `prePatch` hook
    - Enable `strictDeps`
    - Disable redundant `patchShebangs` call (already done in `prePatch`
      hook)
    - Address lints from `nixpkgs-hammer`
    - Reformat with `nixfmt-rfc-style`
    - Migrate to pkgs/by-name
    
    [^1]: https://developer.apple.com/library/archive/documentation/FileManagement/Conceptual/APFS_Guide/FAQ/FAQ.html

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6880,8 +6880,6 @@ with pkgs;
 
   convfont = callPackage ../tools/misc/convfont { };
 
-  convmv = callPackage ../tools/misc/convmv { };
-
   cpcfs = callPackage ../tools/filesystems/cpcfs { };
 
   coreutils =  callPackage ../tools/misc/coreutils { };
