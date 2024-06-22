a56e6484f68992222a1e17c14dfb2576f23b0516
seth <getchoo@tuta.io>
2024-06-21 20:29:49 -0400

    efibootmgr: adopt; modernize (#319159)
    * efibootmgr: migrate to by-name
    
    * efibootmgr: format with nixfmt
    
    * efibootmgr: adopt
    
    * efibootmgr: modernize

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7674,8 +7674,6 @@ with pkgs;
 
   emscriptenStdenv = stdenv // { mkDerivation = buildEmscriptenPackage; };
 
-  efibootmgr = callPackage ../tools/system/efibootmgr { };
-
   efivar = callPackage ../tools/system/efivar { };
 
   eget = callPackage ../tools/misc/eget { };
