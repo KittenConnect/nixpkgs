4ac3fbac82430361d09386ac59a0881fe182596a
Anderson Torres <torres.anderson.85@protonmail.com>
2024-07-01 01:37:33 -0300

    ed: a huge rewrite
    - delete `edUnstable`
    - migrate to `pkgs/by-name`
    - strictDeps
    - testVersion
    - meta.mainProgram

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30044,9 +30044,6 @@ with pkgs;
 
   ecs-agent = callPackage ../applications/virtualization/ecs-agent { };
 
-  inherit (recurseIntoAttrs (callPackage ../applications/editors/ed { }))
-    ed edUnstable;
-
   edlin = callPackage ../applications/editors/edlin { };
 
   oed = callPackage ../applications/editors/oed { };
