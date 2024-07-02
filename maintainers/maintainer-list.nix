476774095f9ac3230e740537d75cfbdf261ccacc
Lily Ballard <lily@ballards.net>
2024-07-02 12:20:31 -0700

    maintainers: remove lilyball

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11444,12 +11444,6 @@
     githubId = 54189319;
     name = "Lilly Cham";
   };
-  lilyball = {
-    email = "lily@sb.org";
-    github = "lilyball";
-    githubId = 714;
-    name = "Lily Ballard";
-  };
   limeytexan = {
     email = "limeytexan@gmail.com";
     github = "limeytexan";
