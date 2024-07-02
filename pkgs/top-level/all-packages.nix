9303b54707dd91df0cf1c017afcd3a905fd4a290
Quentin Frey <51170829+Limosine@users.noreply.github.com>
2024-07-02 19:09:55 +0200

    vnote: 3.17.0 -> 3.18.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -39875,7 +39875,7 @@ with pkgs;
     buildGoModule = buildGo122Module;
   };
 
-  vnote = libsForQt5.callPackage ../applications/office/vnote { };
+  vnote = qt6Packages.callPackage ../applications/office/vnote { };
 
   sshportal = callPackage ../servers/sshportal { };
 
