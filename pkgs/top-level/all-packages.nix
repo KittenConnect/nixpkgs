14998c2935955e041f1e11764867e76952f5450a
Jalil David Salamé Messina <jalil.salame@gmail.com>
2024-07-04 11:02:40 +0200

    mdbook: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9710,8 +9710,6 @@ with pkgs;
 
   md2gemini = with python3.pkgs; toPythonApplication md2gemini;
 
-  mdbook = callPackage ../tools/text/mdbook { };
-
   mdbook-emojicodes = callPackage ../tools/text/mdbook-emojicodes { };
 
   mdbook-epub = callPackage ../tools/text/mdbook-epub {
