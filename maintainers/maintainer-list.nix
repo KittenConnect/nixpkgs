e4322b9225ada7ce8e2833ecd026c9b9abab9665
Gary Guo <gary@garyguo.net>
2024-07-04 17:29:39 +0100

    maintainers: add rvfg and garyguo
    Co-authored-by: Rvfg <i@rvf6.com>

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -7609,6 +7609,12 @@
     githubId = 91987;
     name = "Jim Garrison";
   };
+  garyguo = {
+    email = "gary@garyguo.net";
+    github = "nbdd0121";
+    githubId = 4065244;
+    name = "Gary Guo";
+  };
   gavin = {
     email = "gavin.rogers@holo.host";
     github = "gavinrogers";
@@ -18709,6 +18715,12 @@
     github = "R-VdP";
     githubId = 141248;
     name = "Ramses";
+  };
+  rvfg = {
+    email = "i@rvf6.com";
+    github = "duament";
+    githubId = 30264485;
+    name = "Rvfg";
   };
   rvl = {
     email = "dev+nix@rodney.id.au";
