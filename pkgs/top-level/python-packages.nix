bc3a8eb03919690b95210436551f016a763c30f7
natsukium <tomoya.otabi@gmail.com>
2024-07-07 13:55:07 +0900

    python312Packages.graspologic-native: init at 1.2.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5221,6 +5221,8 @@ self: super: with self; {
 
   graspologic = callPackage ../development/python-modules/graspologic { };
 
+  graspologic-native = callPackage ../development/python-modules/graspologic-native { };
+
   greatfet = callPackage ../development/python-modules/greatfet { };
 
   greeclimate = callPackage ../development/python-modules/greeclimate { };
