e24fa13abae80795c32d77eb002cfcbd0dbf76c7
t4ccer <t4ccer@gmail.com>
2024-07-10 13:32:26 -0600

    sratoolkit: build from source

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17460,8 +17460,6 @@ with pkgs;
 
   srandrd = callPackage ../tools/X11/srandrd { };
 
-  sratoolkit = callPackage ../applications/science/biology/sratoolkit { };
-
   srecord = callPackage ../development/tools/misc/srecord { };
 
   srelay = callPackage ../tools/networking/srelay { };
