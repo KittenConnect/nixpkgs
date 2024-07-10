dc3a63fb138470322abd48917af0ccbe20518cb4
tahanonu <tochipreciousahan@gmail.com>
2024-07-10 20:44:02 +0100

    python312Package.ssort: init at 0.13.0
    Release: https://github.com/bwhmather/ssort/releases/tag/0.13.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14720,6 +14720,8 @@ self: super: with self; {
 
   ssg = callPackage ../development/python-modules/ssg { };
 
+  ssort = callPackage ../development/python-modules/ssort { };
+
   stack-data = callPackage ../development/python-modules/stack-data { };
 
   stanio = callPackage ../development/python-modules/stanio { };
