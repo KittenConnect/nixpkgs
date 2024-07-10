66b2cb29a77474809d1db8e4a59f413c4e6d30a6
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2024-07-10 10:23:47 +1000

    conform: unpin buildGoModule

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2182,9 +2182,7 @@ with pkgs;
 
   commitlint = nodePackages."@commitlint/cli";
 
-  conform = callPackage ../applications/version-management/conform {
-    buildGoModule = buildGo122Module;
-  };
+  conform = callPackage ../applications/version-management/conform { };
 
   datalad = callPackage ../applications/version-management/datalad { };
 
