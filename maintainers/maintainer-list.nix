5222f6c6b8b2bba7f7de5035879c306f2573a805
Crem <maxcaminer1@gmail.com>
2024-07-10 16:36:12 +1000

    maintainers: add crem

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4124,6 +4124,12 @@
     githubId = 34543609;
     name = "creator54";
   };
+  crem = {
+    email = "max@alstercaminer.com";
+    github = "MaxCaminer";
+    githubId = 16345077;
+    name = "crem";
+  };
   crertel = {
     email = "chris@kedagital.com";
     github = "crertel";
