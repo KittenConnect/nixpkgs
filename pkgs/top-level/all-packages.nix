b62477d5e8d1433690e77394c297349bcf707d68
Will Cohen <willcohen@users.noreply.github.com>
2024-07-12 14:59:26 -0400

    emscripten: 3.1.55 -> 3.1.64

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7596,7 +7596,7 @@ with pkgs;
   easeprobe = callPackage ../tools/misc/easeprobe { };
 
   emscripten = callPackage ../development/compilers/emscripten {
-    llvmPackages = llvmPackages_18;
+    llvmPackages = llvmPackages_git;
   };
 
   emscriptenPackages = recurseIntoAttrs (callPackage ./emscripten-packages.nix { });
