6d12a6c300c31860d9df034135b67e6d4f552e6d
Peder Bergebakken Sundt <pbsds@hotmail.com>
2024-07-13 16:44:46 +0200

    python311Packages.objexplore: init at 1.5.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9215,6 +9215,8 @@ self: super: with self; {
 
   objax = callPackage ../development/python-modules/objax { };
 
+  objexplore = callPackage ../development/python-modules/objexplore { };
+
   objsize = callPackage ../development/python-modules/objsize { };
 
   objgraph = callPackage ../development/python-modules/objgraph {
