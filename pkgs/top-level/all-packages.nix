e438031f16049781aa566839ab62c12d3c3b9763
Nikolay Korotkiy <sikmir@disroot.org>
2024-07-15 13:43:24 +0400

    geographiclib: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20603,8 +20603,6 @@ with pkgs;
 
   geogram = callPackage ../development/libraries/geogram { };
 
-  geographiclib = callPackage ../development/libraries/geographiclib { };
-
   geoip = callPackage ../development/libraries/geoip { };
 
   geoipjava = callPackage ../development/libraries/java/geoipjava { };
