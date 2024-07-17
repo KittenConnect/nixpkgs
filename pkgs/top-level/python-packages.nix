bda4c99f45a7753231a77b351df62592d5629369
Roshan Kumar <58213083+Roshaen@users.noreply.github.com>
2024-07-17 13:51:42 +0530

    python312Packages.python-ffmpeg: init at 2.0.12 (#320185)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10310,6 +10310,8 @@ self: super: with self; {
 
   python-ecobee-api = callPackage ../development/python-modules/python-ecobee-api { };
 
+  python-ffmpeg = callPackage ../development/python-modules/python-ffmpeg { };
+
   python-flirt = callPackage ../development/python-modules/python-flirt { };
 
   python-fullykiosk = callPackage ../development/python-modules/python-fullykiosk { };
