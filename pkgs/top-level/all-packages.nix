89d3ddd7ff9071b7d8851db30244952ba808d2c2
zimbatm <zimbatm@zimbatm.com>
2024-07-25 21:26:09 +0200

    ovh-ttyrec: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11310,8 +11310,6 @@ with pkgs;
 
   oxlint = callPackage ../development/tools/oxlint { };
 
-  ovh-ttyrec = callPackage ../tools/misc/ovh-ttyrec { };
-
   ovito = libsForQt5.callPackage ../applications/graphics/ovito {
     inherit (darwin.apple_sdk.frameworks) VideoDecodeAcceleration;
   };
