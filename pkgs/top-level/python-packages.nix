3f4d4fd09235b1d19e079ece14c4a48eec109db9
Sigmanificient <edhyjox@gmail.com>
2024-07-26 05:07:01 +0200

    python312Packages.yt-dlp-dearrow: init at 2023.01.01

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -17646,6 +17646,8 @@ self: super: with self; {
     python3Packages = self;
   });
 
+  yt-dlp-dearrow = callPackage ../development/python-modules/yt-dlp-dearrow { };
+
   youtube-search = callPackage ../development/python-modules/youtube-search { };
 
   youtube-search-python = callPackage ../development/python-modules/youtube-search-python { };
