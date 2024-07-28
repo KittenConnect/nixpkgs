d0076fd54fd88d1cf77967f3d267378ada08aa1d
Tobias Mayer <tobim@fastmail.fm>
2024-07-28 16:33:28 +0200

    rapidjson: 1.1.0 -> unstable-2024-04-09, rapidjson-unstable: drop
    The last oficial release of rapidjson is 8 years old, development has
    continued without releases since then. The old version is affected
    by CVE-2024-38517.
    
    https://www.opencve.io/cve/CVE-2024-38517

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23697,8 +23697,6 @@ with pkgs;
 
   rapidjson = callPackage ../development/libraries/rapidjson { };
 
-  rapidjson-unstable = callPackage ../development/libraries/rapidjson/unstable.nix { };
-
   rapidxml = callPackage ../development/libraries/rapidxml { };
 
   rapidyaml = callPackage ../development/libraries/rapidyaml {};
