d5699039ed3503624528e4ae7e02d03f2174eb3c
Ryan Omasta <git@ryand.ca>
2024-08-01 16:20:34 -0600

    scrcpy: 2.5 -> 2.6; migrate to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12463,8 +12463,6 @@ with pkgs;
     inherit (darwin.apple_sdk.libs) utmp;
   };
 
-  scrcpy = callPackage ../misc/scrcpy { };
-
   screen-message = callPackage ../tools/X11/screen-message { };
 
   screenkey = callPackage ../applications/video/screenkey { };
