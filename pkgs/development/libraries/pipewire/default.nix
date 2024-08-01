a5346bf6622abb72c8bc0c59a4f30775c9e3ff0a
K900 <me@0upti.me>
2024-08-01 12:38:15 +0300

    pipewire: 1.2.1 -> 1.2.2
    Diff: https://gitlab.freedesktop.org/pipewire/pipewire/-/compare/1.2.1...1.2.2
    
    Changelog: https://gitlab.freedesktop.org/pipewire/pipewire/-/releases/1.2.2

--- pkgs/development/libraries/pipewire/default.nix
+++ pkgs/development/libraries/pipewire/default.nix
@@ -65,7 +65,7 @@
 
 stdenv.mkDerivation(finalAttrs: {
   pname = "pipewire";
-  version = "1.2.1";
+  version = "1.2.2";
 
   outputs = [
     "out"
@@ -81,7 +81,7 @@ stdenv.mkDerivation(finalAttrs: {
     owner = "pipewire";
     repo = "pipewire";
     rev = finalAttrs.version;
-    sha256 = "sha256-CkxsVD813LbWpuZhJkNLJnqjLF6jmEn+CajXb2XTCsY=";
+    sha256 = "sha256-neLQ41p2f2QyOS3r2VxanaHbiVj6nnnkT7kx/On0azM=";
   };
 
   patches = [
