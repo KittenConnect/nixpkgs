e075c730afbd79d1914d463f0f0c540832bedcad
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-08-03 23:53:19 +0200

    rectangle: move to pkgs/by-name, format

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27217,8 +27217,6 @@ with pkgs;
 
   reap = callPackage ../os-specific/linux/reap { };
 
-  rectangle = callPackage ../os-specific/darwin/rectangle { };
-
   regionset = callPackage ../os-specific/linux/regionset { };
 
   rfkill_udev = callPackage ../os-specific/linux/rfkill/udev.nix { };
