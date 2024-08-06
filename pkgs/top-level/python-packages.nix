180fc6b3d4ffb562776ee218ed244f02f4dc7179
Robert Schütz <mail@dotlambda.de>
2024-08-06 05:01:23 -0700

    python312Packages.pyemoncms: init at 0.1.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11304,6 +11304,8 @@ self: super: with self; {
 
   pyemd = callPackage ../development/python-modules/pyemd { };
 
+  pyemoncms = callPackage ../development/python-modules/pyemoncms { };
+
   pyemvue = callPackage ../development/python-modules/pyemvue { };
 
   pyenchant = callPackage ../development/python-modules/pyenchant {
