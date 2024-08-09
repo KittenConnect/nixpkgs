7738409e70e7b0c4d9b725a5324118be5da6b146
Peder Bergebakken Sundt <pbsds@hotmail.com>
2024-08-09 03:33:08 +0200

    python312Packages.reflex: init at 0.5.9

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13654,6 +13654,8 @@ self: super: with self; {
 
   refery = callPackage ../development/python-modules/refery { };
 
+  reflex = callPackage ../development/python-modules/reflex { };
+
   reflex-hosting-cli = callPackage ../development/python-modules/reflex-hosting-cli { };
 
   reflink = callPackage ../development/python-modules/reflink { };
