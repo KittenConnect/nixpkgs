2bdadf2ba4f43caeb66e38df8bcbda7afcaa2814
Luflosi <luflosi@luflosi.de>
2024-08-10 12:52:10 +0200

    npiet: cleanup
    - Move to pkgs/by-name
    - Format
    - Use `finalAttrs` instead of `rec` for passing the `version` to `fetchurl`
    - Use `hash` instead of `sha256` for `fetchurl`
    - Use `--replace-fail` instead of `--replace` for `substituteInPlace`
    - Add `strictDeps = true`
    - Use `lib` directly in `meta` instead of using `with lib;`
    - Add `meta.changelog`
    - Add `meta.mainProgram`

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10837,8 +10837,6 @@ with pkgs;
 
   npapi_sdk = callPackage ../development/libraries/npapi-sdk { };
 
-  npiet = callPackage ../development/interpreters/npiet { };
-
   npth = callPackage ../development/libraries/npth { };
 
   nmap-formatter = callPackage ../tools/security/nmap-formatter { };
