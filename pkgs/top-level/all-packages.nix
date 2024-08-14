42b344822591e0e26897a17c37f8d5642cf1fea3
zowoq <59103226+zowoq@users.noreply.github.com>
2024-08-14 10:57:10 +1000

    nix-eval-jobs: 2.23.0 -> 2.24.0
    Diff: https://github.com/nix-community/nix-eval-jobs/compare/v2.23.0...v2.24.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -38730,7 +38730,7 @@ with pkgs;
   dnadd = callPackage ../tools/nix/dnadd { };
 
   nix-eval-jobs = callPackage ../tools/package-management/nix-eval-jobs {
-    nix = nixVersions.nix_2_22;
+    nix = nixVersions.nix_2_24;
   };
 
   nix-doc = callPackage ../tools/package-management/nix-doc { };
