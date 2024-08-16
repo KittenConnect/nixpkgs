b2b3e1e5e49ce8e49930c67d7d1abe78e39fe932
HaskellHegemonie <znerol@mail.de>
2024-08-17 00:50:02 +0200

    maintainers: add HaskellHegemonie
    Signed-off-by: HaskellHegemonie <znerol@mail.de>

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -8025,6 +8025,13 @@
     githubId = 62279738;
     name = "Håkon Arvidsen";
   };
+  HaskellHegemonie = {
+    name = "HaskellHegemonie";
+    email = "haskellisierer@proton.me";
+    github = "HaskellHegemonie";
+    githubId = 73712423;
+    keys = [ { fingerprint = "A559 0A2A 5B06 1923 3917  5F13 5622 C205 6513 577B"; } ];
+  };
   haslersn = {
     email = "haslersn@fius.informatik.uni-stuttgart.de";
     github = "haslersn";
