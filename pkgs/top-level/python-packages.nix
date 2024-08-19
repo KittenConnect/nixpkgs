66184e5eb5b046c78530d7ce5d76e3afd4ae7fb4
Ben Darwin <bcdarwin@gmail.com>
2024-08-19 12:46:25 -0400

    python312Packages.pyzx: init at 0.8.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13318,6 +13318,8 @@ self: super: with self; {
 
   pyzmq = callPackage ../development/python-modules/pyzmq { };
 
+  pyzx = callPackage ../development/python-modules/pyzx { };
+
   qbittorrent-api = callPackage ../development/python-modules/qbittorrent-api { };
 
   qasync = callPackage ../development/python-modules/qasync { };
