a288324c66a72aed72ba3c00b1bf8140b1ee03f6
Jason Odoom <jasonodoom@gmail.com>
2024-08-20 01:57:20 -0400

    python312Packages.reprint: init at 0.6.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13585,6 +13585,8 @@ self: super: with self; {
 
   reproject = callPackage ../development/python-modules/reproject { };
 
+  reprint = callPackage ../development/python-modules/reprint { };
+
   reprshed = callPackage ../development/python-modules/reprshed { };
 
   reptor = callPackage ../development/python-modules/reptor { };
