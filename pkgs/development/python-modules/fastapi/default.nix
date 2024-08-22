2d29677cc817ac772220de549d99ace75aed65f2
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-08-22 12:45:09 +0200

    python312Packages.fastapi: disable flaky test

--- pkgs/development/python-modules/fastapi/default.nix
+++ pkgs/development/python-modules/fastapi/default.nix
@@ -105,6 +105,8 @@ buildPythonPackage rec {
   disabledTests = [
     # Coverage test
     "test_fastapi_cli"
+    # ResourceWarning: Unclosed <MemoryObjectSendStream>
+    "test_openapi_schema"
   ];
 
   disabledTestPaths = [
