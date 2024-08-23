9e143c7e5a106c236f8475b39afe4dd838bd996a
Anderson Torres <torres.anderson.85@protonmail.com>
2024-08-23 12:20:54 -0300

    isc-cron: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6901,7 +6901,7 @@ with pkgs;
 
   cromfs = callPackage ../tools/archivers/cromfs { };
 
-  cron = callPackage ../tools/system/cron { };
+  cron = isc-cron;
 
   ctlptl = callPackage ../development/tools/ctlptl { };
 
