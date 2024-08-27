5dd991f55ec66b45f0481ccc2bea50a533d95c4d
Jan Likar <jan.likar@protonmail.com>
2024-08-27 20:26:55 +0200

    maintainers: add janlikar

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -9259,6 +9259,12 @@
     github = "jankaifer";
     githubId = 12820484;
   };
+  janlikar = {
+    name = "Jan Likar";
+    email = "jan.likar@protonmail.com";
+    github = "janlikar";
+    githubId = 4228250;
+  };
   jansol = {
     email = "jan.solanti@paivola.fi";
     github = "jansol";
