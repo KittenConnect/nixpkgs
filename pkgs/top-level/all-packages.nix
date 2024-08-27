af344bf4a27d9e57f831bcb401bc56fff37de8d4
Emily <vcs@emily.moe>
2024-08-27 22:52:44 +0100

    treewide: optimistically unpin FFmpeg 7 dependencies
    Per the adjusted FFmpeg pinning advice, packages that work on the
    default version should use the unversioned variants to ease the
    migration to future versions and reduce the number of packages that
    end up referencing old versions.
    
    I have left HandBrake pinned as it builds a custom patched FFmpeg.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5605,9 +5605,7 @@ with pkgs;
 
   medusa = callPackage ../tools/security/medusa { };
 
-  megasync = libsForQt5.callPackage ../applications/misc/megasync {
-    ffmpeg = ffmpeg_7;
-  };
+  megasync = libsForQt5.callPackage ../applications/misc/megasync { };
 
   megacmd = callPackage ../applications/misc/megacmd { };
 
