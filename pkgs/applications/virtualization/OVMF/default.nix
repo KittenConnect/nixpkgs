afe7da99ceaa5383112f46fcda24acda0ddbc84b
Martin Joerg <martin.joerg@gmail.com>
2024-08-27 15:14:51 +0000

    OVMF: enable compilation using Clang
    Disable -Wno-unneeded-internal-declaration causing the build to fail due to -Wall with Clang.

--- pkgs/applications/virtualization/OVMF/default.nix
+++ pkgs/applications/virtualization/OVMF/default.nix
@@ -194,6 +194,6 @@ edk2.mkDerivation projectDscPath (finalAttrs: {
     license = lib.licenses.bsd2;
     platforms = metaPlatforms;
     maintainers = with lib.maintainers; [ adamcstephens raitobezarius mjoerg ];
-    broken = stdenv.isDarwin;
+    broken = stdenv.isDarwin && stdenv.isAarch64;
   };
 })
