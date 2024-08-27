11541916f15b5705315cbcae91ac2551cce1d6bc
aleksana <me@aleksana.moe>
2024-08-27 16:57:18 +0800

    video-trimmer: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13689,8 +13689,6 @@ with pkgs;
     pythonPackages = python3Packages;
   };
 
-  video-trimmer = callPackage ../applications/video/video-trimmer { };
-
   via = callPackage ../tools/misc/via { };
 
   vial = callPackage ../tools/misc/vial { };
