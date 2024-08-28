5d0e74b54bfe5a976468c21a61be3747c0ceb822
claes <claes@holmerson.net>
2024-08-28 19:08:10 +0200

    maintainers: add claes

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3813,6 +3813,12 @@
     githubId = 199180;
     name = "Claes Wallin";
   };
+  claes = {
+    email = "claes.holmerson@gmail.com";
+    github = "claes";
+    githubId = 43564;
+    name = "Claes Holmerson";
+  };
   clebs = {
     email = "borja.clemente@gmail.com";
     github = "clebs";
