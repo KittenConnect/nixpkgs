2483eef6177d1c2b0bd8722ec6883eb5c01cdd49
Nikolay Korotkiy <sikmir@disroot.org>
2024-09-01 15:46:31 +0400

    openfst: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -39448,8 +39448,6 @@ with pkgs;
 
   houdini = callPackage ../applications/misc/houdini { };
 
-  openfst = callPackage ../development/libraries/openfst { };
-
   opengrm-ngram = callPackage ../development/libraries/opengrm-ngram { };
 
   openring = callPackage ../applications/misc/openring { };
