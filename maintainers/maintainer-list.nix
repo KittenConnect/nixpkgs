e7ab451ce298016c78ad754ef1bdd2fc5ba7e24a
hellodword <46193371+hellodword@users.noreply.github.com>
2024-09-02 01:31:10 +0000

    maintainers: add hellodword

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -8208,6 +8208,11 @@
     githubId = 2405974;
     name = "Sébastian Méric de Bellefon";
   };
+  hellodword = {
+    github = "hellodword";
+    githubId = 46193371;
+    name = "hellodword";
+  };
   hellwolf = {
     email = "zhicheng.miao@gmail.com";
     github = "hellwolf";
