7278bf3f232ec121706d2f3d7974ce386a535a41
Anthony ROUSSEL <anthony@roussel.dev>
2024-09-03 20:43:41 +0200

    python311Packages.python-troveclient: init at 7.0.0
    Add [Trove Client package](https://github.com/openstack/python-troveclient),
    a client for the OpenStack Trove DBaaS service.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22966,6 +22966,7 @@ with pkgs;
   manilaclient = with python311Packages; toPythonApplication python-manilaclient;
   mistralclient = with python311Packages; toPythonApplication python-mistralclient;
   swiftclient = with python311Packages; toPythonApplication python-swiftclient;
+  troveclient = with python311Packages; toPythonApplication python-troveclient;
 
   openvdb = callPackage ../development/libraries/openvdb { };
 
