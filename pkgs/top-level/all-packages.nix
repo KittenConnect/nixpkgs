d5048b1567a65514a51f56b1cbfe08ac86b5922a
Sergei Trofimovich <slyich@gmail.com>
2024-09-10 22:07:47 +0100

    texinfo5: remove package, old version
    The only user was `gcc48`. Drop `texinfo5` as it's an old unsupported
    branch of the package.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19015,7 +19015,6 @@ with pkgs;
   texinfoPackages = callPackages ../development/tools/misc/texinfo/packages.nix { };
   inherit (texinfoPackages)
     texinfo413
-    texinfo5 # needed for gcc48
     texinfo6_5 # needed for allegro
     texinfo6_7 # needed for gpm, iksemel and fwknop
     texinfo6
