881f502b8da08fe45d74a3433c422cbb1a49d12d
wxt <3264117476@qq.com>
2024-09-11 10:51:45 +0800

    nixos/turn-rs: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -857,6 +857,7 @@
   ./services/misc/tautulli.nix
   ./services/misc/tiddlywiki.nix
   ./services/misc/tp-auto-kbbl.nix
+  ./services/misc/turn-rs.nix
   ./services/misc/tuxclocker.nix
   ./services/misc/transfer-sh.nix
   ./services/misc/tzupdate.nix
