b4e4bcc4c3c394e7e68358a9d3e0d0aa049b4d10
KFearsoff <66781795+KFearsoff@users.noreply.github.com>
2024-09-16 02:57:14 +0400

    maintainers: remove kfears (#342128)
    remove kfears from maintainers

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10890,13 +10890,6 @@
     githubId = 8211181;
     name = "Kevin Kandlbinder";
   };
-  kfears = {
-    email = "kfearsoff@gmail.com";
-    github = "KFearsoff";
-    githubId = 66781795;
-    matrix = "@kfears:matrix.org";
-    name = "KFears";
-  };
   kfollesdal = {
     email = "kfollesdal@gmail.com";
     github = "kfollesdal";
