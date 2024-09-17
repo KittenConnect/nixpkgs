4d775a6bdc3d9dd83583105d8a7437cd7db44643
Anthony ROUSSEL <anthony@roussel.dev>
2024-09-17 20:55:52 +0200

    sonic-server: migrate to pkgs/by-name, format with nixfmt-rfc-style

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8313,8 +8313,6 @@ with pkgs;
 
   sbsigntool = callPackage ../tools/security/sbsigntool { };
 
-  sonic-server = callPackage ../servers/search/sonic-server { };
-
   gsmartcontrol = callPackage ../tools/misc/gsmartcontrol { };
 
   gsmlib = callPackage ../development/libraries/gsmlib
