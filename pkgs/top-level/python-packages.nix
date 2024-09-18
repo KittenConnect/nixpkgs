6616bc3bd4e528be3e77560e02f7cf72073b5f75
Robert Schütz <mail@dotlambda.de>
2024-09-18 12:32:13 -0700

    python312Packages.qcs-api-client-common: init at 0.10.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13136,6 +13136,8 @@ self: super: with self; {
 
   qcs-api-client = callPackage ../development/python-modules/qcs-api-client { };
 
+  qcs-api-client-common = callPackage ../development/python-modules/qcs-api-client-common { };
+
   qcs-sdk-python = callPackage ../development/python-modules/qcs-sdk-python { };
 
   qdarkstyle = callPackage ../development/python-modules/qdarkstyle { };
