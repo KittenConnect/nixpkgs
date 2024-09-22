a771278f4e15f6651c1e1b81fd47c678b456ddbc
NotAShelf <raf@notashelf.dev>
2024-09-22 08:58:42 +0300

    waycorner: move to by-name overlay; format via nixfmt

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30064,8 +30064,6 @@ with pkgs;
 
   remontoire = callPackage ../applications/misc/remontoire { };
 
-  waycorner = callPackage ../applications/misc/waycorner { };
-
   wayshot = callPackage ../tools/misc/wayshot { };
 
   waylevel = callPackage ../tools/misc/waylevel { };
