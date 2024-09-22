266c64fd7a443e66d0085e7f00bf79fd4edc2018
Mutsuha Asada <me@momee.mt>
2024-09-22 18:01:53 +0900

    algolia-cli: move to by-name and modernize derivation - Removed `with lib;` - Formatted via nixfmt - Removed `rec`

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6207,8 +6207,6 @@ with pkgs;
 
   byzanz = callPackage ../applications/video/byzanz { };
 
-  algolia-cli = callPackage ../development/tools/algolia-cli { };
-
   anydesk = callPackage ../applications/networking/remote/anydesk { };
 
   anystyle-cli = callPackage ../tools/misc/anystyle-cli { };
