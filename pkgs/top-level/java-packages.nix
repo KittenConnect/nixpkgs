6defb650b4cf4df1ba244487d2be9e79de75b99d
Emily <vcs@emily.moe>
2024-09-24 17:51:28 +0100

    {openjdk,openjfx}22: drop

--- pkgs/top-level/java-packages.nix
+++ pkgs/top-level/java-packages.nix
@@ -7,11 +7,10 @@ let
   openjfx15 = callPackage ../development/compilers/openjdk/openjfx/15 { };
   openjfx17 = callPackage ../development/compilers/openjdk/openjfx/17 { };
   openjfx21 = callPackage ../development/compilers/openjdk/openjfx/21 { };
-  openjfx22 = callPackage ../development/compilers/openjdk/openjfx/22 { };
   openjfx23 = callPackage ../development/compilers/openjdk/openjfx/23 { };
 
 in {
-  inherit openjfx11 openjfx17 openjfx21 openjfx22 openjfx23;
+  inherit openjfx11 openjfx17 openjfx21 openjfx23;
 
   compiler = let
 
@@ -102,17 +101,10 @@ in {
         openjfx = openjfx21;
       };
 
-    openjdk22 = mkOpenjdk
-      ../development/compilers/openjdk/22.nix
-      ../development/compilers/zulu/22.nix
-      {
-        openjdk22-bootstrap = temurin-bin.jdk-22;
-        openjfx = openjfx22;
-      };
-
     openjdk23 = mkOpenjdk
       ../development/compilers/openjdk/23.nix
       ../development/compilers/zulu/23.nix
+      {
         openjdk23-bootstrap = temurin-bin.jdk-23;
         openjfx = openjfx23;
       };
