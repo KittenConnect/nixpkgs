a407c314f8193e45c0023b4337db5b49f9d4f2ba
wxt <3264117476@qq.com>
2024-09-29 17:00:10 +0800

    garble: move to by-name; nixfmt

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19467,8 +19467,6 @@ with pkgs;
 
   ganv = callPackage ../development/libraries/ganv { };
 
-  garble = callPackage ../development/tools/garble { };
-
   gcab = callPackage ../development/libraries/gcab { };
 
   gcovr = with python3Packages; toPythonApplication gcovr;
