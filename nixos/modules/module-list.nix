abbd6acd8dad39bc2259d7d181dfe0200e6c26c4
h7x4 <h7x4@nani.wtf>
2024-09-29 21:40:39 +0200

    nixos/bluemap: move to `web-apps`

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1384,6 +1384,7 @@
   ./services/web-apps/atlassian/crowd.nix
   ./services/web-apps/atlassian/jira.nix
   ./services/web-apps/audiobookshelf.nix
+  ./services/web-apps/bluemap.nix
   ./services/web-apps/bookstack.nix
   ./services/web-apps/c2fmzq-server.nix
   ./services/web-apps/calibre-web.nix
@@ -1515,7 +1516,6 @@
   ./services/web-apps/zitadel.nix
   ./services/web-servers/agate.nix
   ./services/web-servers/apache-httpd/default.nix
-  ./services/web-servers/bluemap.nix
   ./services/web-servers/caddy/default.nix
   ./services/web-servers/darkhttpd.nix
   ./services/web-servers/fcgiwrap.nix
