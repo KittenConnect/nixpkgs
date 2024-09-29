974afa103837adeb2a8b2494743af9833e85170e
Morten Munk <mortenmunk97@gmail.com>
2024-09-29 19:19:02 +0200

    maintainers: add mortenmunk

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -14260,6 +14260,12 @@
     githubId = 42215704;
     name = "Moritz Böhme";
   };
+  mortenmunk = {
+    email = "mortenmunk97@gmail.com";
+    github = "MortenMunk";
+    githubId = 92527083;
+    name = "Morten Munk";
+  };
   MostAwesomeDude = {
     email = "cds@corbinsimpson.com";
     github = "MostAwesomeDude";
