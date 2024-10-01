76df06ee545fdf507145394408a69b7b58de4464
Merlin Humml <megoettlinger@gmail.com>
2024-10-01 12:49:36 +0200

    typos: move to by-name and formatting

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13225,8 +13225,6 @@ with pkgs;
 
   typesense = callPackage ../servers/search/typesense { };
 
-  typos = callPackage ../development/tools/typos { };
-
   tz = callPackage ../tools/misc/tz { };
 
   u9fs = callPackage ../servers/u9fs { };
