3629736a76a99203b55c341cbd092c12ea25caff
Mutsuha Asada <me@momee.mt>
2024-10-01 14:19:12 +0900

    buildpack: moved to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4168,8 +4168,6 @@ with pkgs;
 
   btrbk = callPackage ../tools/backup/btrbk { };
 
-  buildpack = callPackage ../development/tools/buildpack { };
-
   bonk = callPackage ../tools/misc/bonk { };
 
   bottom-rs = callPackage ../tools/misc/bottom-rs { };
