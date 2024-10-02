f04cfd5c04c4fdbaa745de521a8f11349fa8ca17
Gaetan Lepage <gaetan@glepage.com>
2024-10-02 15:35:35 +0200

    slweb: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30211,8 +30211,6 @@ with pkgs;
 
   sleep-on-lan = callPackage ../tools/networking/sleep-on-lan { };
 
-  slweb = callPackage ../applications/misc/slweb { };
-
   sonixd = callPackage ../applications/audio/sonixd { };
 
   sonobus = callPackage ../applications/audio/sonobus { };
