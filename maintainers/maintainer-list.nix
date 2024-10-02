be619c6dd2b9e10b62cef85adf52845fcebb3966
tobifroe <froeltob@pm.me>
2024-10-02 10:10:52 +0200

    maintainers: add tobifroe

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -21932,6 +21932,12 @@
     githubId = 2164118;
     name = "Tobias Bora";
   };
+  tobifroe = {
+    email = "hi@froelich.dev";
+    github = "tobifroe";
+    githubId = 40638719;
+    name = "Tobias Frölich";
+  };
   tobim = {
     email = "nix@tobim.fastmail.fm";
     github = "tobim";
