c5682f4cd43e387d2732e75feb444732aaa47b18
Randy Eckenrode <randy@largeandhighquality.com>
2024-10-03 07:47:30 -0400

    python3: fix stdenv bootstrap on x86_64-darwin
    The bootstrap tools linker sometimes crashes when trying to link the
    sqlite3 tests, which causes the bootstrap Python not to have the sqlite3
    module. This causes the freezegun module to fail to build later in the
    bootstrap. Using the 11.0 SDK fixes the problem.
    
    Upstream Python supports building with a newer SDK and back-deploying,
    so this change should not negatively affect users on pre-11.0 releases.

--- pkgs/development/interpreters/python/cpython/default.nix
+++ pkgs/development/interpreters/python/cpython/default.nix
@@ -26,6 +26,7 @@
 
 # platform-specific dependencies
 , bash
+, apple-sdk_11
 , darwin
 , windows
 
@@ -180,6 +181,9 @@ let
     bluez
   ] ++ optionals enableFramework [
     darwin.apple_sdk.frameworks.Cocoa
+  ] ++ optionals stdenv.hostPlatform.isDarwin [
+    # Work around for ld64 crashes on x86_64-darwin. Remove once 11.0 becomes the default.
+    apple-sdk_11
   ] ++ optionals stdenv.hostPlatform.isMinGW [
     windows.dlfcn
     windows.mingw_w64_pthreads
