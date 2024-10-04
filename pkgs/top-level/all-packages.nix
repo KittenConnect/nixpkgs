5a6847bffd805853918f78bf998e50392908bf6a
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2024-10-04 10:46:35 +0800

    lxgw-wenkai: move to `pkgs/by-name`

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27330,8 +27330,6 @@ with pkgs;
     inherit (plasma5Packages) breeze-icons;
   };
 
-  lxgw-wenkai = callPackage ../data/fonts/lxgw-wenkai { };
-
   maia-icon-theme = libsForQt5.callPackage ../data/icons/maia-icon-theme { };
 
   mailcap = callPackage ../data/misc/mailcap { };
