fa781bf4c515ce4e96f0b0c48893f629e2cc6dc1
musjj <72612857+musjj@users.noreply.github.com>
2024-10-05 20:12:14 +0700

    maintainers: add musjj

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -14831,6 +14831,11 @@
     githubId = 96225281;
     name = "Mustafa Çalışkan";
   };
+  musjj = {
+    name = "musjj";
+    github = "musjj";
+    githubId = 72612857;
+  };
   mvisonneau = {
     name = "Maxime VISONNEAU";
     email = "maxime@visonneau.fr";
