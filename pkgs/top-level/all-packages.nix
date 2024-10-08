b20da6cb12877da6e61279371bcdd176f9ddc848
Benedikt Broich <b.broich@posteo.de>
2024-10-08 11:15:21 +0200

    twitch-cli: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -38113,8 +38113,6 @@ with pkgs;
 
   tvbrowser = callPackage ../applications/misc/tvbrowser { };
 
-  twitch-cli = callPackage ../development/tools/twitch-cli { };
-
   uacme = callPackage ../tools/admin/uacme { };
 
   ufiformat = callPackage ../tools/system/ufiformat { };
