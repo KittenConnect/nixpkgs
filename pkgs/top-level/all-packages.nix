0c38ba17f1e4964ed1457a8ce0c48cff9b94d245
Anderson Torres <torres.anderson.85@protonmail.com>
2024-10-13 11:05:52 -0300

    vym: use standard callPackage

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -33413,7 +33413,7 @@ with pkgs;
 
   vwm = callPackage ../applications/window-managers/vwm { };
 
-  vym = libsForQt5.callPackage ../applications/misc/vym { };
+  vym = callPackage ../applications/misc/vym { };
 
   wad = callPackage ../tools/security/wad { };
 
