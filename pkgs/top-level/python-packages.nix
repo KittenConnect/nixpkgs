a9b7bb707a7a8f65ae5247cbb44d342f30c46fd1
Gaetan Lepage <gaetan@glepage.com>
2024-10-14 18:32:54 +0200

    snakemake: 8.20.1 -> 8.23.0
    Diff:
    https://github.com/snakemake/snakemake/compare/refs/tags/v8.20.1...v8.23.0
    
    Changelog:
    https://github.com/snakemake/snakemake/blob/refs/tags/v8.23.0/CHANGELOG.md

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14542,7 +14542,7 @@ self: super: with self; {
   });
 
   snakemake = toPythonModule (pkgs.snakemake.override {
-    python3 = python;
+    python3Packages = self;
   });
 
   snakemake-executor-plugin-cluster-generic = callPackage ../development/python-modules/snakemake-executor-plugin-cluster-generic { };
