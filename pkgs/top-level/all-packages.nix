51d8b79efb22b9d5be6ca86fa949cf9895e52b41
Shaw Vrana <shaw@vranix.com>
2024-10-16 12:17:30 -0700

    juicefs: fix build
    Resolves #349073

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8831,7 +8831,9 @@ with pkgs;
 
   jmespath = callPackage ../development/tools/jmespath { };
 
-  juicefs = callPackage ../tools/filesystems/juicefs { };
+  juicefs = callPackage ../tools/filesystems/juicefs {
+    buildGoModule = buildGo122Module;
+  };
 
   juicity = callPackage ../tools/networking/juicity { };
 
