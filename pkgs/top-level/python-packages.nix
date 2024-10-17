2ba040fedf248227c5a8f3ffedf13bd264213ead
Nick Cao <nickcao@nichi.co>
2024-10-17 17:07:31 -0400

    python312Packages.quamash: drop

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13333,8 +13333,6 @@ self: super: with self; {
 
   qualysclient = callPackage ../development/python-modules/qualysclient { };
 
-  quamash = callPackage ../development/python-modules/quamash { };
-
   quandl = callPackage ../development/python-modules/quandl { };
 
   quantile-forest = callPackage ../development/python-modules/quantile-forest { };
