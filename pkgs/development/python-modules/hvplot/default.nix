17ff37497b3995a33a334365d6b45498c774f793
Doron Behar <doron.behar@gmail.com>
2024-10-20 21:00:03 +0300

    python312Packages.hvplot: 0.10.0 -> 0.11.1
    Changelog: https://github.com/holoviz/hvplot/releases/tag/v0.11.1

--- pkgs/development/python-modules/hvplot/default.nix
+++ pkgs/development/python-modules/hvplot/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "hvplot";
-  version = "0.11.0";
+  version = "0.11.1";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-8796HdWF5P2j6XhUUxxD+5IBV7LFALnmPdNkAf/igDQ=";
+    hash = "sha256-mJ7QOJGJrcR+3NJgHS6rGL82bnSwf14oc+AhMjxKFLs=";
   };
 
   build-system = [ setuptools-scm ];
