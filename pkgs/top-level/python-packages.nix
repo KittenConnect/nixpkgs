5d118d6b5e7175fa44ad80f2e968f6b071c4f087
Gaetan Lepage <gaetan@glepage.com>
2024-10-21 08:11:16 +0200

    python312Packages.pesq: init at 0.0.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9896,6 +9896,8 @@ self: super: with self; {
 
   pescea = callPackage ../development/python-modules/pescea { };
 
+  pesq = callPackage ../development/python-modules/pesq { };
+
   pex = callPackage ../development/python-modules/pex { };
 
   pexif = callPackage ../development/python-modules/pexif { };
