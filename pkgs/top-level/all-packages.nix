9d907fb0b608797be004198cb6856a0f6688a76b
aleksana <me@aleksana.moe>
2024-10-22 14:33:09 +0800

    gtklock-playerctl-module: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14407,8 +14407,6 @@ with pkgs;
 
   gpxsee = gpxsee-qt5;
 
-  gtklock-playerctl-module = callPackage ../tools/wayland/gtklock/playerctl-module.nix { };
-
   gtklock-powerbar-module = callPackage ../tools/wayland/gtklock/powerbar-module.nix { };
 
   gtklock-userinfo-module = callPackage ../tools/wayland/gtklock/userinfo-module.nix { };
