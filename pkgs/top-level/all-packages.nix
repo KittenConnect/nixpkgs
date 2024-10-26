10bf4a23257c9f9a9f069aa508068a6bdf8caf9b
Payas Relekar <payas@relekar.org>
2024-10-26 20:18:22 +0530

    gleam: Add erlang_27 as runtime dependency
    While gleam can be run entirely off JS backend, a lot of its intended
    uses are in the backend where its target is BEAM VM of Erlang.
    
    Current Gleam version requires Erlang/OTP 26 (erlang_26), while
    nixpkgs still defaults to Erlang/OTP 25 (erlang). This change fixes
    that.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14801,6 +14801,7 @@ with pkgs;
 
   gleam = callPackage ../development/compilers/gleam {
     inherit (darwin.apple_sdk.frameworks) Security SystemConfiguration;
+    erlang = erlang_27;
   };
 
   gmqcc = callPackage ../development/compilers/gmqcc { };
