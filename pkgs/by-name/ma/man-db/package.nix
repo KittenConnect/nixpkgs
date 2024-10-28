3ac54687e8332e2a9b7c4ac45147b051472bfe08
Philipp Jungkamp <p.jungkamp@gmx.net>
2024-10-28 11:46:47 +0100

    man-db: Add `--with-col` to `configureFlags`
    Piping the output of man to a file (not a tty) is supposed to strip
    the formatting (ANSI escape sequences) from the output text if the
    MAN_KEEP_FORMATTING environment variable is undefined.
    
    Stripping of ansi escape sequences is done using `col` from `util-linux`. the
    build system uses the value of the `--with-col` flag or tries to find `col`
    in `path` during the build.  if no `col` is found or specified the documented
    stripping of ansi sequences won't be performed.
    
    Add the `--with-col` flag to fix piping of man pages as plain text.
    
    Use `util-linuxMinimal` to avoid a cyclic dependency.

--- pkgs/by-name/ma/man-db/package.nix
+++ pkgs/by-name/ma/man-db/package.nix
@@ -11,6 +11,7 @@
 , nixosTests
 , pkg-config
 , stdenv
+, util-linuxMinimal
 , zstd
 }:
 
@@ -61,6 +62,7 @@ stdenv.mkDerivation rec {
     "--with-systemdtmpfilesdir=${placeholder "out"}/lib/tmpfiles.d"
     "--with-systemdsystemunitdir=${placeholder "out"}/lib/systemd/system"
     "--with-pager=less"
+    "--with-col=${util-linuxMinimal}/bin/col"
   ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
     "ac_cv_func__set_invalid_parameter_handler=no"
     "ac_cv_func_posix_fadvise=no"
