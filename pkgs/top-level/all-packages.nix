37ee4edcde574087e246631f13d30e809c53300b
Tomo <tomodachi94@protonmail.com>
2024-10-28 07:13:07 +0000

    exodus: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -33942,8 +33942,6 @@ with pkgs;
 
   erigon = callPackage ../applications/blockchains/erigon { };
 
-  exodus = callPackage ../applications/blockchains/exodus { };
-
   faraday = callPackage ../applications/blockchains/faraday { };
 
   fulcrum = libsForQt5.callPackage ../applications/blockchains/fulcrum { };
