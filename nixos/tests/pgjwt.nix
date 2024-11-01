f112b1665ec2e43edff9068d1597be61d8bdd285
Wolfgang Walther <walther@technowledgy.de>
2024-11-01 19:34:26 +0100

    postgresqlPackages.pgjwt: make passthru.tests work with correct package
    Same reasoning as commit before.

--- nixos/tests/pgjwt.nix
+++ nixos/tests/pgjwt.nix
@@ -1,35 +1,50 @@
-import ./make-test-python.nix ({ pkgs, lib, ...}:
+{ system ? builtins.currentSystem
+, config ? {}
+, pkgs ? import ../.. { inherit system config; }
+}:
 
-with pkgs; {
-  name = "pgjwt";
-  meta = with lib.maintainers; {
-    maintainers = [ spinus willibutz ];
-  };
+with import ../lib/testing-python.nix { inherit system pkgs; };
 
-  nodes = {
-    master = { ... }:
-    {
-      services.postgresql = {
-        enable = true;
-        extraPlugins = ps: with ps; [ pgjwt pgtap ];
+let
+  inherit (pkgs) lib;
+
+  makePgjwtTest = postgresqlPackage:
+    makeTest {
+      name = "pgjwt-${postgresqlPackage.name}";
+      meta = with lib.maintainers; {
+        maintainers = [ spinus willibutz ];
       };
-    };
-  };
 
-  testScript = { nodes, ... }:
-  let
-    sqlSU = "${nodes.master.services.postgresql.superUser}";
-    pgProve = "${pkgs.perlPackages.TAPParserSourceHandlerpgTAP}";
-    inherit (nodes.master.services.postgresql.package.pkgs) pgjwt;
-  in
-  ''
-    start_all()
-    master.wait_for_unit("postgresql")
-    master.succeed(
-        "${pkgs.gnused}/bin/sed -e '12 i SET search_path TO tap,public;' ${pgjwt.src}/test.sql > /tmp/test.sql"
-    )
-    master.succeed(
-        "${pkgs.sudo}/bin/sudo -u ${sqlSU} PGOPTIONS=--search_path=tap,public ${pgProve}/bin/pg_prove -d postgres -v -f /tmp/test.sql"
-    )
-  '';
-})
+      nodes = {
+        master = { ... }:
+        {
+          services.postgresql = {
+            enable = true;
+            package = postgresqlPackage;
+            extraPlugins = ps: with ps; [ pgjwt pgtap ];
+          };
+        };
+      };
+
+      testScript = { nodes, ... }:
+      let
+        sqlSU = "${nodes.master.services.postgresql.superUser}";
+        pgProve = "${pkgs.perlPackages.TAPParserSourceHandlerpgTAP}";
+        inherit (nodes.master.services.postgresql.package.pkgs) pgjwt;
+      in
+      ''
+        start_all()
+        master.wait_for_unit("postgresql")
+        master.succeed(
+            "${pkgs.gnused}/bin/sed -e '12 i SET search_path TO tap,public;' ${pgjwt.src}/test.sql > /tmp/test.sql"
+        )
+        master.succeed(
+            "${pkgs.sudo}/bin/sudo -u ${sqlSU} PGOPTIONS=--search_path=tap,public ${pgProve}/bin/pg_prove -d postgres -v -f /tmp/test.sql"
+        )
+      '';
+    };
+in
+lib.concatMapAttrs (n: p: { ${n} = makePgjwtTest p; }) pkgs.postgresqlVersions
+// {
+  passthru.override = p: makePgjwtTest p;
+}
