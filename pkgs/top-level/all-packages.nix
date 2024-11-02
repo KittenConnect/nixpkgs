37a7903972e17e9a7f4604c69cdfca2eab729705
Tomo <tomodachi94@protonmail.com>
2024-11-02 18:11:06 +0000

    sqldeveloper: drop
    sqldeveloper was dropped due to being severely out-of-date and
    having a dependency on JavaFX for Java 8, which we do not support.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15714,10 +15714,6 @@ with pkgs;
 
   spirv-llvm-translator = callPackage ../development/compilers/spirv-llvm-translator { };
 
-  sqldeveloper = callPackage ../development/tools/database/sqldeveloper {
-    jdk = oraclejdk;
-  };
-
   sqlfluff = callPackage ../development/tools/database/sqlfluff { };
 
   sqlx-cli = callPackage ../development/tools/rust/sqlx-cli {
